4598bc1d1deca5c42e6807ef7c0b3337ed21d4be
Munib <139039102+Munib78@users.noreply.github.com>
2024-07-30 20:54:25 +0530

    Update test_eight.py

--- test_eight.py
+++ test_eight.py
@@ -2,16 +2,16 @@ import eight
 
 
 def test_nums_with_multiple_delimiter():
-    # Test when the input string uses a semicolon as a delimiter
+    # Test when the input string uses 2 types of delimiter
     result = eight.add("//[*][%]\n1*2%3")
     assert result == 6
 
 def test_nums_with_string_delimiter():
-    # Test when the input string uses tab characters as delimiters
+    # Test when the input string uses deleminator of more than lenght 1 as delimiters
     result = eight.add("//[***]\n1***2***3")
     assert result == 6
 
 def test_nums_with_multiples_delimiter():
-    # Test when the input string uses a semicolon as a delimiter
+    # Test when the input string uses 3 types of delimiter
     result = eight.add("//[*][%][>]\n1*2%3>4")
     assert result == 10
